982deed62fade2692bdb46dcbd2a4ee68fb7d4b6
Przemek <pwalczykk@gmail.com>
2016-08-08 10:55:49 +0200

    baud 19200

--- phobos_shared/include/UART_Shared.hpp
+++ phobos_shared/include/UART_Shared.hpp
@@ -3,7 +3,7 @@
 
 #include <termios.h>
 #define SHARED_C_CC_VMIN 1
-#define SHARED_C_CFLAG B115200 | CS8 | CLOCAL | CREAD
+#define SHARED_C_CFLAG B19200 | CS8 | CLOCAL | CREAD
 #define SHARED_C_IFLAG IGNPAR
 #define SHARED_C_OFLAG 0
 #define SHARED_C_LFLAG 0
