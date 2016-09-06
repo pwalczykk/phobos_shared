55e4af34c5ed7ec23a59798075df6d60eb093215
Przemek <pwalczykk@gmail.com>
2016-09-06 18:11:52 +0200

    conversions compilation

--- phobos_shared/include/Conversions.hpp
+++ phobos_shared/include/Conversions.hpp
@@ -1,7 +1,7 @@
 #ifndef MY_CONVERSIONS_HPP_
 #define MY_CONVERSIONS_HPP_
-
-namespace conv{
+//
+// namespace conv{
 
     ///////////////////////////////// POSITION
 
@@ -29,12 +29,12 @@ namespace conv{
 
     ///////////////////////////////// ORIENTATION
 
-    float ORIENT_MAX 1;
-    float ORIENT_MIN -1;
+    float ORIENT_MAX = 1;
+    float ORIENT_MIN = -1;
 
-    float ORIENT_RESOLUTION 0.001;
-    float ORIENT_INTMAX (ORIENT_MAX / ORIENT_RESOLUTION);
-    float ORIENT_INTMIN (ORIENT_MIN / ORIENT_RESOLUTION);
+    float ORIENT_RESOLUTION = 0.001;
+    float ORIENT_INTMAX = (ORIENT_MAX / ORIENT_RESOLUTION);
+    float ORIENT_INTMIN = (ORIENT_MIN / ORIENT_RESOLUTION);
 
     int Orient_Float2Int(float input){
         if(input > ORIENT_MAX) input = ORIENT_MAX;
@@ -52,47 +52,47 @@ namespace conv{
 
     ////////////////////////////////// WHEELS_VEL
 
-    float WHEELS_VEL_FLOAT_MAX = 1;
-    float WHEELS_VEL_INT_MAX = 256;
+    // float WHEELS_VEL_FLOAT_MAX = 1;
+    // float WHEELS_VEL_INT_MAX = 256;
+    //
+    // int WheelsVel_Float2Int(float input){
+    //     if(input > WHEELS_VEL_FLOAT_MAX) input = WHEELS_VEL_FLOAT_MAX;
+    //     if(input < -WHEELS_VEL_FLOAT_MAX) input = -WHEELS_VEL_FLOAT_MAX;
+    //
+    //     return (int) (input * WHEELS_VEL_INT_MAX / WHEELS_VEL_FLOAT_MAX);
+    // }
+    //
+    // float WHEELS_IMPS_PER_ROT = 1000;
+    //
+    // float WheelsVel_Imps2Rots(int impulses){
+    //     return ((float) impulses) / WHEELS_IMPS_PER_ROT;
+    // }
+    //
+    // float WheelsVel_Imps2Rads(int impulses){
+    //     return ((float) impulses) / WHEELS_IMPS_PER_ROT * 2 * 3.1416;
+    // }
+    //
+    // ////////////////////////////////// ARM_VEL
+    //
+    // float LINK_VEL_FLOAT_MAX[6];
+    // float LINK_VEL_INT_MAX[6];
+    //
+    // LINK_VEL_FLOAT_MAX[0] = 1;
+    // LINK_VEL_FLOAT_MAX[1] = 1;
+    // LINK_VEL_FLOAT_MAX[2] = 1;
+    // LINK_VEL_FLOAT_MAX[3] = 1;
+    // LINK_VEL_FLOAT_MAX[4] = 1;
+    // LINK_VEL_FLOAT_MAX[5] = 1;
+    //
+    // LINK_VEL_INT_MAX[0] = 256;
+    // LINK_VEL_INT_MAX[1] = 256;
+    // LINK_VEL_INT_MAX[2] = 256;
+    // LINK_VEL_INT_MAX[3] = 256;
+    // LINK_VEL_INT_MAX[4] = 256;
+    // LINK_VEL_INT_MAX[5] = 256;
+    //
+    // float ArmVel_
 
-    int WheelsVel_Float2Int(float input){
-        if(input > WHEELS_VEL_FLOAT_MAX) input = WHEELS_VEL_FLOAT_MAX;
-        if(input < -WHEELS_VEL_FLOAT_MAX) input = -WHEELS_VEL_FLOAT_MAX;
-
-        return (int) (input * WHEELS_VEL_INT_MAX / WHEELS_VEL_FLOAT_MAX);
-    }
-
-    float WHEELS_IMPS_PER_ROT = 1000;
-
-    float WheelsVel_Imps2Rots(int impulses){
-        return ((float) impulses) / WHEELS_IMPS_PER_ROT;
-    }
-
-    float WheelsVel_Imps2Rads(int impulses){
-        return ((float) impulses) / WHEELS_IMPS_PER_ROT * 2 * 3.1416;
-    }
-
-    ////////////////////////////////// ARM_VEL
-
-    float LINK_VEL_FLOAT_MAX[6];
-    float LINK_VEL_INT_MAX[6];
-
-    LINK_VEL_FLOAT_MAX[0] = 1;
-    LINK_VEL_FLOAT_MAX[1] = 1;
-    LINK_VEL_FLOAT_MAX[2] = 1;
-    LINK_VEL_FLOAT_MAX[3] = 1;
-    LINK_VEL_FLOAT_MAX[4] = 1;
-    LINK_VEL_FLOAT_MAX[5] = 1;
-
-    LINK_VEL_INT_MAX[0] = 256;
-    LINK_VEL_INT_MAX[1] = 256;
-    LINK_VEL_INT_MAX[2] = 256;
-    LINK_VEL_INT_MAX[3] = 256;
-    LINK_VEL_INT_MAX[4] = 256;
-    LINK_VEL_INT_MAX[5] = 256;
-
-    float ArmVel_
-
-}
+// }
 
 #endif
